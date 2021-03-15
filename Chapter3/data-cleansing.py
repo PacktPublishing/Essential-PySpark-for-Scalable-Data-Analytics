629e8ecc7c5ddac56654476be8addd0650edd96d
Sreeram Nudurupati <snudurupati@users.noreply.github.com>
2021-03-15 06:32:09 +0530

    Delete data-cleansing.py

--- Chapter3/data-cleansing.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-# MAGIC %fs ls FileStore/shared_uploads/enriched_retail
-
-# COMMAND ----------
-
-retail_enriched = spark.read.option("header", True).csv("FileStore/shared_uploads/enriched_retail")
-retail_enriched.createOrReplaceTempView("retail_enriched")
